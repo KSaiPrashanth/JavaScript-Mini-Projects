9826001f98e7fa4a9958c07e2f974692f1023e18
Sai Prashanth K <171444765+KSaiPrashanth@users.noreply.github.com>
2024-10-07 20:18:43 +0530

    modified js file in 06-To-Do List App

--- 06-To-Do List App/script.js
+++ 06-To-Do List App/script.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
   if (inputBox.value === "") {
-    alert("You must write soomthhing");
+    alert("You must write something");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
